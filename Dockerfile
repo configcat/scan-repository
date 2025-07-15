7380619c88da3ea272f4e06ea429adc4e8aaf70f
Peter Csajtai <peter.csajtai@outlook.com>
2025-07-15 18:04:59 +0200

    Use v2.4.2 CLI / add timeout option

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM configcat/cli:2.4.0
+FROM configcat/cli:2.4.2
 COPY entrypoint.sh /entrypoint.sh
 RUN ["chmod", "+x", "/entrypoint.sh"]
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -9,9 +9,10 @@ configcat scan "$GITHUB_WORKSPACE/$SCAN_SUB_DIR" \
     --config-id=${SCAN_CONFIG_ID} \
     --repo=${GITHUB_REPOSITORY} \
     --line-count=${SCAN_LINE_COUNT} \
+    --timeout=${SCAN_TIMEOUT} \
     --file-url-template="https://github.com/$GITHUB_REPOSITORY/blob/{commitHash}/{filePath}#L{lineNumber}" \
     --commit-url-template="https://github.com/$GITHUB_REPOSITORY/commit/{commitHash}" \
-    --runner="ConfigCat GitHub Action v2.7.0" \
+    --runner="ConfigCat GitHub Action v2.7.1" \
     --upload \
     --verbose=${SCAN_VERBOSE} \
     --non-interactive \
